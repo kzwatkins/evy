324f78dafcf1c3964a40565db26a18350f205b79
Kera Z. Watkins <kzw@acm.org>
2017-07-04 15:19:02 -0400

    The timer seems to be working!

--- evy/ViewController.h
+++ evy/ViewController.h
@@ -8,8 +8,45 @@
 
 #import <UIKit/UIKit.h>
 
+enum
+{
+    
+    NUM_FOLLOWUP_DAYS = 180,
+    HR_MAX = 24,
+    MIN_MAX = 60,
+    SEC_MAX = 60,
+    DAY_MAX = 365,
+    NUM_FOLLOWUP_SEC = NUM_FOLLOWUP_DAYS * HR_MAX * MIN_MAX * SEC_MAX
+};
+
+NSTimer* timer;
+//NSRunLoop *runner;
+int count = 0;
+
 @interface ViewController : UIViewController
 
+// Initial work
+- (void) initPress;
+
+//
+- (void) printCountdown: (int) days hrTens: (int) hrTens hrOnes: (int) hrOnes minTens: (int) minTens minOnes: (int) minOnes secTens: (int) secTens secOnes: (int) secOnes;
+
+// Handling timing events.
+- (void) createTimer: (int) secInterval;
+- (void) updateTimeView;
+- (void) updateTimeShown;
+- (void) updateTimeText: (int) maxTime timeLen: (int)timeLen txtTensPlace:(UITextField*) tensPlace txtOnesPlace:(UITextField*) onesPlace;
+- (void) stopTimer;
+- (NSString*) getTimeSnapshot;
+
+// Conversions
+- (int) secToMins: (int) sec;
+- (int) secToHrs: (int) sec;
+- (int) secToDays: (int) sec;
+
+// The countdown has completed.
+- (void) postCountdown;
+- (void) countdownDone;
 
 @end
 
--- evy/ViewController.m
+++ evy/ViewController.m
@@ -9,6 +9,19 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *txtDays;
+
+@property (weak, nonatomic) IBOutlet UITextField *txtHrTens;
+@property (weak, nonatomic) IBOutlet UITextField *txtHrOnes;
+
+@property (weak, nonatomic) IBOutlet UITextField *txtMinTens;
+@property (weak, nonatomic) IBOutlet UITextField *txtMinOnes;
+
+@property (weak, nonatomic) IBOutlet UITextField *txtSecTens;
+@property (weak, nonatomic) IBOutlet UITextField *txtSecOnes;
+
+@property (weak, nonatomic) IBOutlet UILabel *txtTimeSnapshot;
+@property (weak, nonatomic) IBOutlet UIButton *btnCountdown;
 
 @end
 
@@ -23,6 +36,136 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)btnCountdown:(id)sender {
+    
+    int days = self.txtDays.text.intValue;
+    
+    int hrTens = self.txtHrTens.text.intValue;
+    int hrOnes = self.txtHrOnes.text.intValue;
+    
+    int minTens = self.txtMinTens.text.intValue;
+    int minOnes = self.txtMinOnes.text.intValue;
+    
+    int secTens = self.txtSecTens.text.intValue;
+    int secOnes = self.txtSecOnes.text.intValue;
+    
+    [self printCountdown: days hrTens: hrTens hrOnes: hrOnes minTens: minTens minOnes: minOnes secTens: secTens secOnes: secOnes];
+    
+   // 1) Initial btn press handling
+    if(self.btnCountdown.isEnabled){
+        [self initPress];
+        [self createTimer:1];
+    }
+}
+
+//: Modified from https://looksok.wordpress.com/2012/12/29/ios-timer-countdown-implementation-tutorial-including-source-code/
+
+- (void) createTimer: (int) secInterval {
+    
+    timer = [NSTimer scheduledTimerWithTimeInterval:secInterval
+        target:self
+        selector:@selector(updateTimeView)
+        userInfo:nil
+        repeats:YES ];
+ 
+    NSRunLoop* runner = [NSRunLoop currentRunLoop];
+    [runner addTimer:timer forMode: NSDefaultRunLoopMode];
+
+}
+
+
+- (void) updateTimeView {
+//    NSDate *startDate = [[timer userInfo] objectForKey:@"StartDate"];
+//    
+//    NSLog(@"Timer started on %@", startDate);
+    
+    count--;
+    self.txtTimeSnapshot.text = [NSString stringWithFormat:@"%d", count];
+    [self updateTimeShown];
+    
+    if(count <= 0){
+        [self postCountdown];
+    }
+}
+
+- (void) updateTimeShown {
+    
+    [self updateTimeText:SEC_MAX timeLen:count txtTensPlace:self.txtSecTens txtOnesPlace:self.txtSecOnes];
+    [self updateTimeText:MIN_MAX timeLen:[self secToMins:count] txtTensPlace:self.txtMinTens txtOnesPlace:self.txtMinOnes];
+    [self updateTimeText:HR_MAX timeLen:[self secToHrs:count] txtTensPlace:self.txtHrTens txtOnesPlace:self.txtHrOnes];
+    [self updateDays:count];
+}
+
+- (void) updateDays: (int) seconds{
+    int days = [self secToDays:seconds];
+    days = days % DAY_MAX;
+    [self.txtDays setText:[NSString stringWithFormat:@"%d", days]];
+}
+
+- (int) secToDays: (int) sec {
+    int result = sec / SEC_MAX / MIN_MAX / HR_MAX;
+    return result;
+}
+
+- (int) secToMins: (int) sec {
+    int result = sec / SEC_MAX;
+    return result;
+}
+
+- (int) secToHrs: (int) sec {
+    int result = sec / SEC_MAX / MIN_MAX ;
+    return result;
+}
+
+- (void) updateTimeText: (int) maxTime timeLen: (int)timeLen txtTensPlace:(UITextField*) tensPlace txtOnesPlace:(UITextField*) onesPlace{
+
+//    int totalTime = count * maxTime;
+//    int time = count % maxTime;
+    NSLog (@"PreTime(s): %d", timeLen);
+    timeLen = timeLen % maxTime;
+    NSLog (@"PostTime(s): %d", timeLen);
+   
+    [tensPlace setText:[NSString stringWithFormat:@"%d", timeLen / 10]];
+    [onesPlace setText:[NSString stringWithFormat:@"%d", timeLen % 10]];
+   
+}
+
+- (void) postCountdown {
+    [self stopTimer];
+    [self countdownDone];
+}
+
+- (void) stopTimer {
+    if(timer){
+        [timer invalidate];
+        timer = nil;
+    }
+}
+
+- (void)countdownDone {
+    [self.btnCountdown setTitle:@"Countdown Done" forState:UIControlStateNormal];
+   
+    NSLog(@"Countdown is done!");
+}
+
+- (void)printCountdown: (int) days hrTens: (int) hrTens hrOnes: (int) hrOnes minTens: (int) minTens minOnes: (int) minOnes secTens: (int) secTens secOnes: (int) secOnes{
+    NSLog(@"days - %d, hours - %d:%d, minutes - %d:%d, seconds - %d:%d", days, hrTens, hrOnes, minTens, minOnes, secTens, secOnes);
+}
+
+- (void) initPress {
+    self.btnCountdown.enabled = NO;
+    [self.btnCountdown setTitle:@"Countdown Begun" forState:UIControlStateNormal];
+    
+    //TODO After testing, change this back to the total seconds.
+    count = NUM_FOLLOWUP_SEC;
+ }
+
+- (NSString*)getTimeSnapshot {
+    long long timeSeconds = (long long)([[NSDate date] timeIntervalSince1970]);
+    
+    return [NSString stringWithFormat:@"%lld", timeSeconds];
 }
 
 
